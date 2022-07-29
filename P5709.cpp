1badf109e8a84e9f9df9d35315b3634869b4d08a
xiaoyv404 <xiaoyv_404@outlook.com>
2022-07-29 09:39:04 +0000

    P5709

---
+++ P5709.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main() {
+    unsigned int m, t;
+    unsigned long s, allTime;
+    std::cin >> m >> t >> s;
+
+    allTime = m * t;
+    if (t == 0 || allTime <= s) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+
+    std::cout << (int)((allTime - s) / t) << std::endl;
+}

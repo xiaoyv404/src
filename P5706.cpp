ce44256e1e5aa9486f5192b97dbe933c96853d0b
xiaoyv404 <xiaoyv_404@outlook.com>
2022-07-29 06:19:13 +0000

    first init

---
+++ P5706.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main() {
+    double a;
+    unsigned int b;
+    std::cin >> a >> b;
+
+    std::cout << std::setprecision(3) << std::fixed << a / b << std::endl
+              << b * 2 << std::endl;
+
+    return 0;
+}
